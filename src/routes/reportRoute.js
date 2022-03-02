75225c4d4b52871db8e0548970805fc3c9981dd2
ahmadajip55 <pangestuajiahmad@gmail.com>
2022-03-02 19:56:37 +0700

    add id report delete

--- src/routes/reportRoute.js
+++ src/routes/reportRoute.js
@@ -4,6 +4,6 @@ const { reportController } = require('../controllers/index');
 router.get('/', reportController.getReportsByType);
 router.post('/', reportController.saveReport);
 router.post('/download/:id', reportController.downloadReport);
-router.delete('/', reportController.deleteReport);
+router.delete('/:id', reportController.deleteReport);
 
 module.exports = router;
